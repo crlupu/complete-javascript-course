dc619460e4dccbca0e01b72c9c6c9f09d9d75921
Lupu Cristian <lupu.cristian.dev@gmail.com>
2022-03-17 21:00:32 +0200

    add solution for values and variables section

---
+++ 01-Fundamentals-Part-1/assignments/01-values-and-variables/assigment.js
@@ -0,0 +1,6 @@
+let country = "Romania";
+let continent = "Europe";
+let population = 19;
+
+console.log("LECTURE: Values and Variables")
+console.log(`${country}, ${continent}, has a population of ${population} mln. people.`)
