7deeff95b0a3e71a48b2014ebc4e7296881f10e9
Lupu Cristian <lupu.cristian.dev@gmail.com>
2022-03-17 21:18:24 +0200

    add solution for data types section

--- 01-Fundamentals-Part-1/assignments/script.js
+++ 01-Fundamentals-Part-1/assignments/script.js
@@ -4,3 +4,12 @@ let population = 19;
 
 console.log("LECTURE: Values and Variables")
 console.log(`${country}, ${continent}, has a population of ${population} mln. people.`)
+
+let isIsland = false;
+let language;
+
+console.log("LECTURE: Data Types")
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
